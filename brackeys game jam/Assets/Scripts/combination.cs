5cae75fb8f9893b9d60f5b7d78e810145dee60e2
sumanth1827 <sumanth304sv@gmail.com>
2023-02-18 19:28:13 +0530

    added button clue

--- brackeys game jam/Assets/Scripts/combination.cs
+++ brackeys game jam/Assets/Scripts/combination.cs
@@ -8,21 +8,42 @@ public class combination : MonoBehaviour
     //public bool[] puzzle;
     public  int[] sol;
     public Animator anim;
-    public puzzlebutton a, b, c, d;
-
+    //public puzzlebutton[] a, b, c, d;
+    [SerializeField] private puzzlebutton[] a;
+    [SerializeField] bool two, four, six; 
     // Start is called before the first frame update
     void Start()
     {
 
+
     }
 
-    // Update is called once per frame
+    
+    //NOTE: 1 is down, 0 is up
     void Update()
     {
-        //NOTE: 1 is down, 0 is up
-        if (a.zero == sol[0] && b.zero == sol[1] && c.zero == sol[2] && d.zero == sol[3])
+        if(two)
         {
-            anim.SetBool("open door", true);
+            if (a[0].zero == sol[0] && a[1].zero == sol[1])
+            {
+                anim.SetBool("open door", true);
+            }
         }
+        if(four)
+        {
+            if (a[0].zero == sol[0] && a[1].zero == sol[1] && a[2].zero == sol[2] && a[3].zero == sol[3])
+            {
+                anim.SetBool("open door", true);
+            }
+        }
+        if(six)
+        {
+            if (a[0].zero == sol[0] && a[1].zero == sol[1] && a[2].zero == sol[2] && a[3].zero == sol[3] && a[4].zero == sol[4] && a[5].zero == sol[5])
+            {
+                anim.SetBool("open door", true);
+            }
+        }
+        
+
     }
 }
