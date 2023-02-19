aa34c5128a6aaeb5198ec537578d8f2ef10fc86f
PTG1326 <f20212085@goa.bits-pilani.ac.in>
2023-02-19 16:02:47 +0530

    dafafa

--- brackeys game jam/Assets/Scripts/EndTrigger.cs
+++ brackeys game jam/Assets/Scripts/EndTrigger.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndTrigger : MonoBehaviour
 {
+
+    public GameObject panel;
+    public GameObject dog;
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "Player")
         {
             Debug.Log("entered trigger");
+
+            panel.SetActive(true);
+            dog.SetActive(true);
+
+            StartCoroutine(timedelay());
         }
+    }
+
+    IEnumerator timedelay()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        SceneManager.LoadScene(1);
     }
 }
